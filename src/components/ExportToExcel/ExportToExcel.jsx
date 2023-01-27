157045a3e9dff1fbee33a337286041d7237925db
danugarri <danugarri@hotmail.com>
2023-01-27 23:38:20 +0100

    Layout for icon

--- src/components/ExportToExcel/ExportToExcel.jsx
+++ src/components/ExportToExcel/ExportToExcel.jsx
@@ -1,14 +1,21 @@
 import DownloadIcon from '@mui/icons-material/Download';
+import { useEffect, useState } from 'react';
+import { initialState } from '../PetsForm/const';
 import './ExportToExcel.css';
 
-export const ExportToExcel = ({ submitPetsData }) => {
+export const ExportToExcel = ({ bodyData, submitPetsData }) => {
+  const [style, setStyle] = useState();
+
+  useEffect(() => {
+    const handleIconStyle = () => {
+      bodyData !== initialState ? setStyle('downloadIcon') : setStyle('disabled-icon');
+    };
+    handleIconStyle();
+  }, [bodyData]);
+
   return (
     <span>
-      <DownloadIcon
-        onClick={submitPetsData}
-        className='downloadIcon'
-        style={{ fontSize: '100px' }}
-      />
+      <DownloadIcon onClick={submitPetsData} className={style} style={{ fontSize: '100px' }} />
     </span>
   );
 };
